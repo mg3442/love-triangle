5028b9a891ce429332dcf8c875f01135a2162efb
mg3442 <magnusprotas@gmail.com>
2018-09-18 22:05:51 +0300

    second task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,22 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  
+  if (preferences.length < 3) {
+    return 0
+}
+ 
+var triangles = 0;
+for (var i=0; i<preferences.length -2; i++) {
+    
+    var first = preferences[i] ;
+    var second = preferences[first-1];
+    var third  = preferences[second-1];
+
+    if (third == i+1 && first > i+1 && second > i+1){
+        triangles++;
+       
+    }
+}
+return triangles;
 };
